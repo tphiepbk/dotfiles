6337b99105c59db3d793171fa23f8914e1c20fe3
tphiepbk <itmanagertph@gmail.com>
2021-08-08 14:35:21 +0700

    Added aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,15 @@
+alias ll="ls -lah"
+alias cls="clear"
+
+alias desktoppath="cd /mnt/c/Users/itman/Desktop"
+
+alias zshconf="nvim ~/.zshrc"
+alias zshreload="source ~/.zshrc"
+alias aliasconf="nvim $ZSH_CUSTOM/aliases.zsh"
+alias nvimconf="nvim ~/.config/nvim/init.vim"
+
+alias update="sudo apt-get update"
+alias upgrade="sudo apt-get upgrade"
+alias install="sudo apt-get install"
+alias autoremove="sudo apt-get autoremove"
+alias autoclean="sudo apt-get autoclean"
