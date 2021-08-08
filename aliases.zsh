9b579ddab8ed8a45a6367f43171630091d561471
tphiepbk <itmanagertph@gmail.com>
2021-08-08 14:44:11 +0700

    Modified alias

--- aliases.zsh
+++ aliases.zsh
@@ -13,3 +13,12 @@ alias upgrade="sudo apt-get upgrade"
 alias install="sudo apt-get install"
 alias autoremove="sudo apt-get autoremove"
 alias autoclean="sudo apt-get autoclean"
+
+alias gconfname="git config user.name"
+alias gconfemail="git config user.email"
+
+alias gpush="git push"
+alias gadd="git add"
+alias gcommit="git commit -m"
+alias gbranch="git branch"
+alias glogp="git log --pretty=format:\"%h %ad%x09%an%x09%s\" --date=short"
