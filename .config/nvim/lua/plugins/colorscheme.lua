2fe8e40191b425506659a151b7a39d603200799f
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-03-17 18:52:45 +0700

    Change the border color
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
+local orange = "#ff8700"
+
 return {
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      style = "night"
+      style = "night",
+      transparent = true,
+      on_colors = function(colors)
+        colors.border = orange
+      end,
     },
   }
 }
