c4f36dfb2c7450888ed66f1e2fe386b6c02f7daa
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-03-20 20:38:28 +0700

    Change the separator style of bufferline to "slant"
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,11 @@
+-- local blue = '#005f83'
+local blue = '#000050'
 local yellow = '#ffff00'
+local white = '#ffffff'
+
+local background_color = blue
+local tab_text_color = white
 local current_selected_tab_text_color = yellow
-local background_color = '#000050'
 
 return {
   {
@@ -16,15 +21,36 @@ return {
         always_show_bufferline = true,
         show_buffer_close_icons = false,
 				show_close_icon = false,
+        separator_style = "slant",
       },
       highlights = {
+        -- Fill color of the bufferline
         fill = {
             bg = background_color
         },
+
+        -- Tab colors
+        background = {
+            fg = tab_text_color,
+        },
+        buffer_visible = {
+            fg = tab_text_color,
+        },
         buffer_selected = {
             fg = current_selected_tab_text_color,
-            bold = true,
+            bold = false,
             italic = false
+        },
+
+        -- Separator colors
+        separator = {
+            fg = background_color,
+        },
+        separator_selected = {
+            fg = background_color,
+        },
+        separator_visible = {
+            fg = background_color,
         },
       }
     }
