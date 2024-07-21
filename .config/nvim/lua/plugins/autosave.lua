9fabcfd6cf70c201ef3e3f120a813174ef40ff52
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-07-21 16:42:17 +0700

    Reduce delay of autosave plugin using debounce_delay option
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -1,11 +1,12 @@
 return {
-  {
-    "okuuva/auto-save.nvim",
-    opts = {
-      execution_message = {
-        enabled = false
-      }
+    {
+        "okuuva/auto-save.nvim",
+        opts = {
+        execution_message = {
+            enabled = false
+        },
+        debounce_delay = 100
+        }
     }
-  }
 }
 
