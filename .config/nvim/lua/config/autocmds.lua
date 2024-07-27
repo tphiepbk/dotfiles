9f2a672ca318e4d735edc38137cf15628e0cd0fe
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-07-27 18:08:10 +0700

    Auto toggle Neotree when neovim starts
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,22 @@
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
 -- Disable autoformat for files
-vim.api.nvim_create_autocmd({ "FileType" }, {
+autocmd({ "FileType" }, {
     pattern = { "*" },
     callback = function()
         vim.b.autoformat = false
+    end,
+})
+
+-- Open NeoTree automatically
+autocmd({ "UiEnter" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.schedule(function()
+            vim.cmd("Neotree")
+        end)
+        vim.schedule(function()
+            vim.cmd("wincmd p")
+        end)
     end,
 })
