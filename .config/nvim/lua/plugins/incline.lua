a4a074209e6358c8b8a519d30934a9a7f126871d
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-07-28 23:55:50 +0700

    Re-design the "incline" statusline
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/incline.lua
+++ .config/nvim/lua/plugins/incline.lua
@@ -1,3 +1,6 @@
+local white = "#ffffff"
+local purple = "#580e8c"
+
 return {
     {
 		"b0o/incline.nvim",
@@ -5,18 +8,17 @@ return {
 		event = "BufReadPre",
 		priority = 1200,
 		config = function()
-			local colors = require("solarized-osaka.colors").setup()
 			require("incline").setup({
 				highlight = {
 					groups = {
-						InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-						InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+						InclineNormal = { guibg = purple, guifg = white },
+						-- InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
 					},
 				},
 				window = {
                     margin = {
                         vertical = 0,
-                        horizontal = 1
+                        horizontal = 0
                     }
                 },
 				hide = {
