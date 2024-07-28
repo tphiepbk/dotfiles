2ed6670761be5488fe5248bb3bbf20876639b6c4
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-07-28 10:40:23 +0700

    Re-tab incline.lua, add space between diagnostic sign and number
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/incline.lua
+++ .config/nvim/lua/plugins/incline.lua
@@ -30,45 +30,45 @@ return {
                     local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
 
                     local function get_git_diff()
-                    local icons = { removed = '', changed = '', added = '' }
-                    local signs = vim.b[props.buf].gitsigns_status_dict
-                    local labels = {}
-                    if signs == nil then
-                        return labels
-                    end
-                    for name, icon in pairs(icons) do
-                        if tonumber(signs[name]) and signs[name] > 0 then
-                        table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+                        local icons = { removed = '', changed = '', added = '' }
+                        local signs = vim.b[props.buf].gitsigns_status_dict
+                        local labels = {}
+                        if signs == nil then
+                            return labels
                         end
-                    end
-                    if #labels > 0 then
-                        table.insert(labels, { '┊ ' })
-                    end
-                    return labels
+                        for name, icon in pairs(icons) do
+                            if tonumber(signs[name]) and signs[name] > 0 then
+                            table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+                            end
+                        end
+                        if #labels > 0 then
+                            table.insert(labels, { '┊ ' })
+                        end
+                        return labels
                     end
 
                     local function get_diagnostic_label()
-                    local icons = { error = '', warn = '', info = '', hint = '' }
-                    local label = {}
+                        local icons = { error = '', warn = '', info = '', hint = '' }
+                        local label = {}
 
-                    for severity, icon in pairs(icons) do
-                        local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-                        if n > 0 then
-                        table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+                        for severity, icon in pairs(icons) do
+                            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+                            if n > 0 then
+                            table.insert(label, { icon .. ' ' .. n .. ' ', group = 'DiagnosticSign' .. severity })
+                            end
                         end
-                    end
-                    if #label > 0 then
-                        table.insert(label, { '┊ ' })
-                    end
-                    return label
+                        if #label > 0 then
+                            table.insert(label, { '┊ ' })
+                        end
+                        return label
                     end
 
                     return {
-                    { get_diagnostic_label() },
-                    { get_git_diff() },
-                    { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
-                    { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' },
-                    { '┊  ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
+                        { get_diagnostic_label() },
+                        { get_git_diff() },
+                        { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
+                        { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' },
+                        { '┊  ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
                     }
 				end,
 			})
