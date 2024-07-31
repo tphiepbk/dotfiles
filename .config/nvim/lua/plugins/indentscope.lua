ca6b1a07e3ce85e021a0d01a5229b7ecf5c72c78
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-07-31 21:07:02 +0700

    Add indentscope plugin which highlights current level of indentation
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

---
+++ .config/nvim/lua/plugins/indentscope.lua
@@ -0,0 +1,10 @@
+return
+{
+    "echasnovski/mini.indentscope",
+    version = false,
+    event = "LazyFile",
+    opts = {
+        symbol = "│",
+        options = { try_as_border = true },
+    },
+}
