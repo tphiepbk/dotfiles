70f3b4a7ece1181df2ab8edb0a47298bb26a2867
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-08-03 10:34:17 +0700

    Change the mapping method from vim to lazy in keymaps.lua
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,51 +6,57 @@
 --   visual_block_mode = "x",
 --   command_mode = "c",
 
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+local lazymap = LazyVim.safe_keymap_set
+
+local opts = { remap = false, silent = true }
 local all_modes = { "n", "i", "v", "x", "t", "c" }
 
 -- Disable arrow keys
-keymap.set(all_modes, "<Up>", "<Nop>", opts)
-keymap.set(all_modes, "<Down>", "<Nop>", opts)
-keymap.set(all_modes, "<Left>", "<Nop>", opts)
-keymap.set(all_modes, "<Right>", "<Nop>", opts)
+lazymap(all_modes, "<Up>", "<Nop>", opts)
+lazymap(all_modes, "<Down>", "<Nop>", opts)
+lazymap(all_modes, "<Left>", "<Nop>", opts)
+lazymap(all_modes, "<Right>", "<Nop>", opts)
 
 -- Disable swapping lines
-keymap.set(all_modes, "<A-j>", "<Nop>", opts)
-keymap.set(all_modes, "<A-k>", "<Nop>", opts)
+lazymap(all_modes, "<A-j>", "<Nop>", opts)
+lazymap(all_modes, "<A-k>", "<Nop>", opts)
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G", opts)
+lazymap("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Remove highlight search
-keymap.set("n", "<Esc>", ":nohl<Return>", opts)
+lazymap({"i", "n"}, "<Esc>", "<cmd>nohl<cr><esc>", opts)
 
 -- Split window
-keymap.set("n", "ss", ":split<CR>")
-keymap.set("n", "sv", ":vsplit<CR>")
+lazymap("n", "ss", "<cmd>split<cr>")
+lazymap("n", "sv", "<cmd>vsplit<cr>")
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sl", "<C-w>l")
-keymap.set("n", "sj", "<C-w>j")
+lazymap("n", "sh", "<C-w>h")
+lazymap("n", "sk", "<C-w>k")
+lazymap("n", "sl", "<C-w>l")
+lazymap("n", "sj", "<C-w>j")
 
 -- Better resize window
 local resize = require("winresize").resize
-keymap.set("n", "<C-Left>", function() resize(0, 2, "left") end, opts)
-keymap.set("n", "<C-Right>", function() resize(0, 2, "right") end, opts)
-keymap.set("n", "<C-Up>", function() resize(0, 1, "up") end, opts)
-keymap.set("n", "<C-Down>", function() resize(0, 1, "down") end, opts)
+lazymap("n", "<C-Left>", function() resize(0, 2, "left") end, opts)
+lazymap("n", "<C-Right>", function() resize(0, 2, "right") end, opts)
+lazymap("n", "<C-Up>", function() resize(0, 1, "up") end, opts)
+lazymap("n", "<C-Down>", function() resize(0, 1, "down") end, opts)
 
 -- Stay in indent mode
-keymap.set("v", "<", "<gv", opts)
-keymap.set("v", ">", ">gv", opts)
+lazymap("v", "<", "<gv", opts)
+lazymap("v", ">", ">gv", opts)
 
 -- Quit and save
-keymap.set("n", "<Leader>qq", ":q<CR>", opts)
-keymap.set("n", "<Leader>qa", ":qa<CR>", opts)
-keymap.set("n", "<Leader>wq", ":wq<CR>", opts)
+lazymap("n", "<Leader>qq", ":q<cr>", opts)
+lazymap("n", "<Leader>qa", ":qa<cr>", opts)
+lazymap("n", "<Leader>wq", ":wq<cr>", opts)
 
 -- Fix bug with "s" key
-keymap.set("n", "s", "<Nop>", opts)
+-- When s is pressed, the previous character is deleted and insert mode is triggered
+lazymap("n", "s", "<Nop>", opts)
+
+-- Delete buffer
+lazymap("n", "<F4>", LazyVim.ui.bufremove, opts)
+
