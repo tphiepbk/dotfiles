3a21654bbd38c2b549d03e5395a1c205edf6ef96
Hiep Phuc Thai <itmanagertph@gmail.com>
2024-08-04 09:19:58 +0700

    Redesign breadcrumb and lualine
    Details:
        - Do not show the "modified" status (dot icon) in breadcrumb
        - Add more icons to lualine
    
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/breadcrumb.lua
+++ .config/nvim/lua/plugins/breadcrumb.lua
@@ -119,7 +119,7 @@ local get_winbar = function()
     end
 
     if not f.isempty(value) and f.get_buf_option "mod" then
-        local mod = "%#LspCodeLens#" .. " " .. "%*"
+        local mod = "%#LspCodeLens#" .. "%*"
         if navic_added then
             value = value .. " " .. mod
         else
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -18,42 +18,67 @@ return {
             globalstatus = true
         },
         sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch' },
+            lualine_a = {
+                {
+                    'mode',
+                    icon = {
+                        '',
+                        align = 'left',
+                        -- color = {
+                        --     fg = 'white'
+                        -- }
+                    }
+                }
+            },
+            lualine_b = {
+                {
+                    'branch',
+                    icon = {
+                        '',
+                        align = 'right',
+                        color = {
+                            fg = 'white'
+                        }
+                    }
+                }
+            },
             lualine_c = {
                 {
                     'filename',
                     file_status = true, -- displays file status (readonly status, modified status)
-                    path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+                    path = 2, -- 0 = just filename, 1 = relative path, 2 = absolute path
+                    icon = {
+                        "",
+                        color = {
+                            fg = 'white'
+                        }
+                    }
                 }
             },
             lualine_x = {
-                { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-                hint = ' ' } },
-                'encoding',
-                'filetype'
-            },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
-        },
-        inactive_sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch' },
-            lualine_c = {
                 {
-                    'filename',
-                    file_status = true, -- displays file status (readonly status, modified status)
-                    path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-                }
-            },
-            lualine_x = {
-                { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-                hint = ' ' } },
+                    'diagnostics',
+                    sources = { "nvim_diagnostic" },
+                    symbols = {
+                        error = ' ',
+                        warn = ' ',
+                        info = ' ',
+                        hint = ' '
+                    }
+                },
                 'encoding',
                 'filetype'
             },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
+            lualine_y = {
+                {
+                    'progress'
+                }
+            },
+            lualine_z = {
+                {
+                    'location'
+                }
+            }
         },
         tabline = {},
         extensions = { 'fugitive' }
