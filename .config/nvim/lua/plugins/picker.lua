1e7bbbf621c481da3df77a96a43720b6ec9612f8
Hiep Phuc Thai <itmanagertph@gmail.com>
2025-09-15 19:39:21 +0700

    Disable Snacks explorer in Neovim
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -1,6 +1,9 @@
 return {
     "folke/snacks.nvim",
     opts = {
+        explorer = {
+            enabled = false
+        },
         keys = {
             { "<leader>ff", function() end, desc = "no thing" },
             { "<leader>fg", function() end, desc = "no thing" },
